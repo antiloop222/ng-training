c2a4e4b06a514597c1dc2067d65485e500f80ef8
Kriss <kriss@vilanet.fr>
2017-09-26 14:42:04 +0100

    Day 2

--- my-app/src/app/catalog.service.ts
+++ my-app/src/app/catalog.service.ts
@@ -8,30 +8,38 @@ export class CatalogService {
 
   private CATALOG:Book[] = [
     {
-        title: "Harry Potter Tome I",
-        author: "J.K. Rowling",
-        isbn: "0172-4976",
-        img: "harry_ecole.jpg",
-        htPrice: 14.90,
-        description: "description"
+      title: "Harry Potter Tome I",
+      author: "Rowling, J.K.",
+      isbn: "0172-4976",
+      img: "harry_ecole.jpg",
+      htPrice: 14.90,
+      description: "description"
     },
     {
-        title: "Harry Potter Tome II",
-        author: "J.K. Rowling",
-        isbn: "8340-6122",
-        img: "harry_prisonnier.jpg",
-        htPrice: 19.90,
-        description: "description"
-      },
+      title: "Harry Potter Tome II",
+      author: "Rowling, J.K.",
+      isbn: "8340-6122",
+      img: "harry_prisonnier.jpg",
+      htPrice: 19.90,
+      description: "description"
+    },
     {
-        title: "Dune Tome I",
-        author: "Frank Herbert",
-        isbn: "1916-2765",
-        img: "dune.jpg",
-        htPrice: 9.90,
-        description: "description"
-      }
-];
+      title: "Dune Tome I",
+      author: "Herbert, Frank",
+      isbn: "1916-2765",
+      img: "dune.jpg",
+      htPrice: 9.90,
+      description: "description"
+    },
+    {
+      title: "Le seigneur des anneaux",
+      author: "Tolkien, J.R.R.",
+      isbn: "8473-1104",
+      img: "anneaux.jpg",
+      htPrice: 21.90,
+      description: "description"
+    }
+  ];
 
 public getCatalog():Book[] {
   return this.CATALOG;
